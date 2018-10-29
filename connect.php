14007b373137c2320a867f3ce3b10fe3ee68280b
jaclynflomen <j_floman@fanshaweonline.ca>
2018-10-28 22:10:30 -0400

    adding back files..something broke which deleted my branch so just reuploading to master

---
+++ connect.php
@@ -0,0 +1,47 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "root";
+$db = "db_infographic";
+
+$conn = mysqli_connect($host, $user, $password, $db);
+
+if (!$conn) {
+    echo "something broke... connection isn't working";
+    exit;
+}
+
+//echo "connected!";
+//comment ^ out once seeing that it works
+
+
+//go and get all data from the database
+//$myQuery = "SELECT * FROM mainmodel";     commenting out to isolate query for one item below
+//$result = mysqli_query($conn, $myQuery);
+//$rows = array ();
+
+//fill the array with the result set and send it to the browser
+//while($row = mysqli_fetch_assoc($result)) {
+//    $rows[] = $row;
+//}
+
+//get one item from database
+if (isset($_GET["region_name"])) {
+    $country = $_GET["region_name"];
+
+    $myQuery = "SELECT * FROM tbl_education WHERE region_id='$country'";
+
+    $result = mysqli_query($conn, $myQuery);
+    $rows = array ();
+
+    //fill the array with the result set and send it to the browser
+    while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+}
+}
+
+//encode the result and send it back
+echo json_encode($rows);
+
+
+?>
