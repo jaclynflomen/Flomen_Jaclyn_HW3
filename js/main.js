cdd2c87665983b378a959c7e2e402e3e87185268
jaclynflomen <j_floman@fanshaweonline.ca>
2018-10-29 20:20:07 -0400

    changing element names

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
     
         function getData(){
     
-            let targetURL = `includes/connect.php?region_nameNo=${this.id}`; //whenever we click on a thumbnail, pass its id to the php query
+            let targetURL = `includes/connect.php?region_name=${this.id}`; //whenever we click on a thumbnail, pass its id to the php query
     
             fetch (targetURL) // go get the data and bring it back! good doggy
             .then(res => res.json()) //turn the result into a plain JS object
@@ -20,13 +20,13 @@
         function showCountrydata(data) {
             debugger;
             //parse the DB info and put it where it needs to go
-            const{ primaryEd, SecondaryEd, YouthLit, Unemployment } = data; //destructuring assignment => MDN JS destructuring
+            const{ primary_ed, secondary_ed, YA_literacy, unemployment } = data; //destructuring assignment => MDN JS destructuring
     
             //grab the elements we need, and populate them with data
-            document.querySelector('.primary_ed').textContent = primaryEd
-            document.querySelector('.secondary_ed').textContent = SecondaryEd
-            document.querySelector('.ya_lit').textContent = YouthLit
-            document.querySelector('.unemployment').textContent = Unemployment
+            document.querySelector('.primary_ed').textContent = primary_ed
+            document.querySelector('.secondary_ed').textContent = secondary_ed
+            document.querySelector('.ya_lit').textContent = YA_literacy
+            document.querySelector('.unemployment').textContent = unemployment
         }
     
         country.forEach(country => country.addEventListener("click", getData));
