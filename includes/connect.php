59a38b88c81b73c6fc910010325e9e3bbe8a9f1e
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-02 21:33:44 -0400

    more testing

--- includes/connect.php
+++ includes/connect.php
@@ -14,11 +14,21 @@ if (!$conn) {
 //echo "connected!";
 //comment ^ out once seeing that it works
 
+//go and get all data from the database
+//$myQuery = "SELECT * FROM mainmodel";     //commenting out to isolate query for one item below
+//$result = mysqli_query($conn, $myQuery);
+//$rows = array ();
+
+//fill the array with the result set and send it to the browser
+//while($row = mysqli_fetch_assoc($result)) {
+//    $rows[] = $row;
+//}
+
 //get one item from database
 if (isset($_GET["region_name"])) {
-    $country = $_GET["region_name"];
+    $car = $_GET["region_name"];
 
-     $myQuery = "SELECT * FROM tbl_education WHERE region_name='$country'";
+     $myQuery = "SELECT * FROM tbl_education WHERE region_name='$car'";
 
     $result = mysqli_query($conn, $myQuery);
     $rows = array ();
--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 (() => {
-    const country = document.querySelectorAll('.data-ref');
+    const car = document.querySelectorAll('.data-ref');
     
         function getData(){
-            
+    
             let targetURL = `includes/connect.php?region_name=${this.id}`; //whenever we click on a thumbnail, pass its id to the php query
     
             fetch (targetURL) // go get the data and bring it back! good doggy
@@ -10,15 +10,16 @@
             .then(data => {
                 console.log(data);
                 //run a function to parse our data
-                showCountryData(data[0]);
+                showCarData(data[0]);
             }) //lets see what we got
             .catch(function(error) {
                 console.log(error); //if anything broke, log it to the console
             });
         }
     
-        function showCountryData(data) {
+        function showCarData(data) {
             //debugger;
+            //parse the DB info and put it where it needs to go
             const{ primary_ed, secondary_ed, YA_literacy, unemployment } = data; //destructuring assignment => MDN JS destructuring
     
             //grab the elements we need, and populate them with data
@@ -28,8 +29,8 @@
             document.querySelector('.unemployment').textContent = unemployment
         }
     
-        country.forEach(country => country.addEventListener("click", getData));
+        car.forEach(car => car.addEventListener("click", getData));
     
-        getData(data); //trigger the getData function
-   
-})();
+        getData(); //trigger the getData function
+    })();
+    
